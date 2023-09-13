67bf413779b0b6919d86cedb13c646fe8d328c67
Kavithamargabandu <kavithamargabandu@gmail.com>
2023-09-13 10:33:41 +0530

    Refactor DVARS calculation in metrics.py
    This commit refactors the `dvars` function in metrics.py to improve error handling
    and readability. Specifically:
    
    - Error handling has been improved by checking for the necessary data attribute
      in the input image and raising a ValueError with a descriptive message if it's missing.
    - The code has been refactored to make it more concise and easier to understand.
    - Comments and docstrings have been updated for clarity.
    
    These changes enhance the reliability and maintainability of the DVARS calculation code.

--- findoutlie/metrics.py
+++ findoutlie/metrics.py
@@ -16,18 +16,17 @@ def dvars(img):
         One-dimensional array with n-1 elements, where n is the number of
         volumes in `img`.
     """
-
-    try:
-        # Get the data from the image.
-        data = img.get_fdata()
-
-        # Calculate differences between consecutive volumes along the time axis.
-        vol_diff = np.diff(data, axis=-1)
-
-        # Calculate spatial RMS (DVARS) for each volume.
-        dvars = np.sqrt(np.mean(vol_diff ** 2, axis=(0, 1, 2)))
-
-        return dvars
-
-    except AttributeError:
+    # Check if the image object has the necessary data attribute.
+    if not hasattr(img, 'get_fdata'):
         raise ValueError("Invalid image: The provided image does not have the necessary data.")
+
+    # Get the data from the image.
+    data = img.get_fdata()
+
+    # Calculate differences between consecutive volumes along the time axis.
+    vol_diff = np.diff(data, axis=-1)
+
+    # Calculate spatial RMS (DVARS) for each volume.
+    dvars = np.sqrt(np.mean(vol_diff ** 2, axis=(0, 1, 2)))
+
+    return dvars
